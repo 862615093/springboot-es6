43c3769a63ebdbd003a9d1e2aaa0105371927386
weiwang127 <weiwang127@iflytek.com>
2022-12-22 10:51:31 +0800

    mq数据同步es

---
+++ src/main/java/com/ww/es/controller/SyncController.java
@@ -0,0 +1,14 @@
+package com.ww.es.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("data/sync/")
+@Api(tags = "数据同步")
+public class SyncController {
+
+
+
+}
